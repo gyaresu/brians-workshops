9a6930d589e2d0efa4f5fd61d858ba4caed13105
Gareth <me@gareth.codes>
2015-06-23 00:36:17 -0700

    Still messing with Google oauth2 implementation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import NavHelper from '../components/nav-helper'
 
 export default React.createClass({
   displayName: 'Home',
   render () {
     const {data} = this.props
     return (
-      <div>
-        <h1>{data}</h1>
-      </div>
+      <NavHelper className='container'>
+        <div>
+          <header role='banner'>
+            <h1>{data}</h1>
+          </header>
+        </div>
+      </NavHelper>
     )
   }
 })
